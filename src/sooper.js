c00cfc31a8e97f0ed712b63b8502bbbb54cd95d9
jonsquared <jon.e.john@gmail.com>
2014-12-28 21:20:28 -0500

    refactoring to reduce code size: several for-->forEach loop conversions and variable usage to reduced repeated references

--- src/sooper.js
+++ src/sooper.js
@@ -9,88 +9,67 @@ var o = Object,
 function define() {
 	var args = arguments,
 		config = args[1] || args[0],
-		fullName = args.length > 1 && args[0];
-	if (config.constructor === Object)
-		config.constructor = (function(){});
+		fullName = args.length > 1 && args[0],
+		constructor = config.constructor;
+	if (constructor === Object)
+		constructor = (function(){});
 	if (fullName)
-		setupClassNamespace(fullName, config);
-	setupSuperClass(config);
-    setupClassInterfaces(config);
-	setupClassProperties(config);
-	return config.constructor;
+		setupClassNamespace(fullName, constructor);
+	setupSuperClass(constructor, config);
+    setupClassInterfaces(constructor, config);
+	setupClassProperties(constructor, config);
+	return constructor;
 }
 
-function getNamespaceFromFullName(fullName) {
+function setupClassNamespace(fullName, constructor) {
 	var names = fullName.split('.'),
+		className = names.pop(),
 		namespace = global;
-	for (var i=0; i<names.length-1; i++) {
-		var nextName = names[i];
-		if (namespace[nextName])
-			namespace = namespace[nextName];
-		else
-			namespace = namespace[nextName] = {};
-	}
-	return namespace;
-}
-
-function setupClassNamespace(fullName, config) {
-	var className = getClassNameFromFullName(fullName),
-		namespace = getNamespaceFromFullName(fullName);
+	names.forEach(function(nextName) {
+		namespace = namespace[nextName] || (namespace[nextName] = {});
+	});
 	//<debug>
 	if (namespace[className])
 		throw new Error('Class "'+className+'" is already defined.');
 	//</debug>
-	namespace[className] = config.constructor;
+	namespace[className] = constructor;
 }
 
-function setupSuperClass(config) {
+function setupSuperClass(constructor, config) {
 	var superClass = config.inherits;
-	if (superClass) {
-		var c = config.constructor,
-			proto = c.prototype = Object.create(superClass.prototype);
-		proto.super = superClass;
-	}
+	if (superClass)
+		(constructor.prototype = Object.create(superClass.prototype)).super = superClass;
 }
 
-function setupClassProperties(config) {
-	var	proto = config.constructor.prototype,
-		props = getOwnPropertyNames(config);
-    for (var i=props.length; i--; ) {
-    	var propName = props[i];
-    	if (propertyNameIsKeyword(propName))
-    		continue;
-		var overriddenPropVal = proto[propName];
-        defineProperty(proto, propName, getOwnPropertyDescriptor(config, propName));
-        if (overriddenPropVal && (proto[propName] instanceof Function))
-        	proto[propName].super = overriddenPropVal;
-	}
-}
-
-function propertyNameIsKeyword(propName) {
-	return propName == "constructor" ||
-		   propName == "inherits" ||
-		   propName == "implements";
-}
-
-function setupClassInterfaces(config) {
-	var interfaces = config.implements;
-	if (!interfaces)
-		return;
-	var proto = config.constructor.prototype;
+function setupClassInterfaces(constructor, config) {
+	var interfaces = config.implements||[];
+	var proto = constructor.prototype;
 	if (!interfaces.push)
 		interfaces = [interfaces];
-	for (var iIndex=0; iIndex<interfaces.length; iIndex++ ) {
-		var iproto = interfaces[iIndex].prototype,
-			iprops = getOwnPropertyNames(iproto);
-		for (var ipropIndex=iprops.length; ipropIndex--; ) {
-			var ipropName = iprops[ipropIndex];
-            defineProperty(proto, ipropName, getOwnPropertyDescriptor(iproto, ipropName));
-        }
-    }
+	interfaces.forEach(function(iFace) {
+		var iproto = iFace.prototype;
+		getOwnPropertyNames(iproto).forEach(function(ipropName) {
+			defineProperty(proto, ipropName, getOwnPropertyDescriptor(iproto, ipropName));
+        });
+    });
 }
 
-function getClassNameFromFullName(fullName) {
-	return fullName.substring(fullName.lastIndexOf('.')+1);
+function setupClassProperties(constructor, config) {
+	var	proto = constructor.prototype,
+		props = getOwnPropertyNames(config);
+    props.forEach(function(propName) {
+    	if (!(propName in {
+				constructor:0,
+				inherits:0,
+				implements:0
+			}))
+		{
+			var overriddenPropVal = proto[propName];
+	        defineProperty(proto, propName, getOwnPropertyDescriptor(config, propName));
+	        if (overriddenPropVal && (proto[propName] instanceof Function))
+	        	proto[propName].super = overriddenPropVal;
+	    }
+	})
 }
 
 module.exports = {
